45ddb33c5bcfb1a0e6150a4e4978adc563b099ad
Niamul Hasan Zead <zead.ece2014@gmail.com>
2022-03-01 11:43:12 +0600

    search field is ready

---
+++ js/api.js
@@ -0,0 +1,5 @@
+const loadSearchValue = () => {
+    const searchField = document.getElementById('search-field');
+    const searchFieldText = searchField.value;
+    console.log(searchFieldText);
+}
