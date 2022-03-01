b3b93477b1cb747f8a8c14e576df1d6f69b4fc4a
Niamul Hasan Zead <zead.ece2014@gmail.com>
2022-03-01 12:35:30 +0600

    api is successfully connected

--- js/api.js
+++ js/api.js
@@ -1,5 +1,19 @@
 const loadSearchValue = () => {
     const searchField = document.getElementById('search-field');
     const searchFieldText = searchField.value;
-    console.log(searchFieldText);
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchFieldText}`;
+    // console.log(url);
+    fetch(url)
+        .then(response => response.json())
+        .then(data => displaySearchResult(data.data));
+}
+
+const displaySearchResult = mobileList => {
+    if (mobileList.length === 0) {
+        console.log('no result found');
+    }
+    else {
+        const maxDisplay = mobileList.slice(0, 20);
+        console.log(maxDisplay);
+    }
 }
